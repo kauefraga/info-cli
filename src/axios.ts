3921628de6ca9b9b2f104d3a190363f9478bfb60
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-01-22 17:05:43 -0300

    :recycle: chore: remove useless api exportation

--- src/axios.ts
+++ src/axios.ts
@@ -1,14 +1,5 @@
 import axios from 'axios';
 
-const quotesAPI = axios.create({
-  baseURL: '',
-});
-
-const weatherAPI = axios.create({
+export const weatherAPI = axios.create({
   baseURL: 'https://api.weatherapi.com/v1',
 });
-
-export {
-  quotesAPI,
-  weatherAPI,
-};
